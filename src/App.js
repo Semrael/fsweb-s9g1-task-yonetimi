8690159cc2788167ffd25b58cf7090a2016ee4b1
semra-elcelik <semraelcelik@gmail.com>
2023-09-18 17:54:55 +0300

    React-Hooh-Form ile oluşturuldu

--- src/App.js
+++ src/App.js
@@ -5,22 +5,39 @@ import TaskForm from "./TaskForm";
 import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
 import { initialTasks, initialTeam } from "./data";
-
+import { toast } from "react-toastify";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
+    toast.success("Başarılı");
   }
 
   function handlePeopleSubmit(yeniKisi) {
-    setTeam([...team, yeniKisi])
+    setTeam([...team, yeniKisi]);
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın")
+    console.log("tamamlama fonksiyonunu buraya yazın");
+    const updatedTasks = tasks.map((item) => {
+      if (item.id === id) {
+        return { ...item, status: "yapıldı" };
+      } else {
+        return item;
+      }
+    });
+    setTasks(updatedTasks);
+    toast.success(`${id} id'li task tamamlandı`);
+
+    const tasksTodo = updatedTasks.filter(
+      (item) => item.status === "yapılacak"
+    );
+    if (tasksTodo.length === 0) {
+      toast.success("Tebrikler, Tüm Göreveri Tamamladınız", { theme: "light" });
+    }
   }
 
   return (
@@ -28,7 +45,7 @@ function App() {
       <div className="formColumn">
         <div className="form-container">
           <h2>Yeni Task</h2>
-          {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
+          {/* {<TaskForm kisiler={team} submitFn={handleTaskSubmit} />} */}
           <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
         </div>
 
@@ -59,7 +76,6 @@ function App() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
